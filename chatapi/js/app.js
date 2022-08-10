8b30cc1fa06e8a8b4e8c341539b45551aeaa20e6
Zorain Ahmed <izorainahmed@gmail.com>
2022-08-10 17:28:48 +0500

    Fifth Commit

--- chatapi/js/app.js
+++ chatapi/js/app.js
@@ -1,20 +1,11 @@
 import {html} from './html.js';
+import {ChatBot} from './funs.js';
 
 $('body').append('<div id="chatbot_id"></div>');
 $('#chatbot_id').append(html);
 
-var datetime = new Date().toLocaleTimeString();
-$('.message-time').append(datetime);
+var func = new ChatBot;
 
-$('.chatbot-icon > img').on('click',function(){
-  $('.chatbot-field').css('display','block');
-  $('.chat-textarea').css('box-shadow','rgb(0 0 0 / 10%) 0px 5px 15px');
-  $('.chatbot-icon').css('display','none');
-});
-
-$('.chat-close').on('click',function(){
-  $('.chatbot-field').css('display','none');
-  $('.chat-textarea').css('box-shadow','none');
-  $('.chatbot-icon').css('display','block');
-});
-  
+func.eventHandler();
+func.addDate();
+func.sendMsg();
--- chatapi/js/funs.js
+++ chatapi/js/funs.js
@@ -1,11 +1,60 @@
-$('.chatbot-icon > img').on('click',function(){
-    $('.chatbot-field').css('display','block');
-    $('.chat-textarea').css('box-shadow','rgb(0 0 0 / 10%) 0px 5px 15px');
-    $('.chatbot-icon').css('display','none');
-  });
+export class ChatBot {
+  constructor(){
+    console.log('Chatbot constructor');
+  }
+
+  eventHandler(){
+    $('.chatbot-icon > img').on('click',function(){
+      $('.chatbot-field').css('display','block');
+      $('.chat-textarea').css('box-shadow','rgb(0 0 0 / 10%) 0px 5px 15px');
+      $('.chatbot-icon').css('display','none');
+    });
+      
+    $('.chat-close').on('click',function(){
+      $('.chatbot-field').css('display','none');
+      $('.chat-textarea').css('box-shadow','none');
+      $('.chatbot-icon').css('display','block');
+    });
+  }
+
+  addDate(){
+    let datetime = new Date().toLocaleTimeString();
+    $('.message-time').append(datetime);
+  }
+
+  sendMsg(){
+    $('.submit-msg').on('click',function(){
+      $('li.chatbot-msg').clone().appendTo('ol.chat-message');
+      $('li:last-child').removeClass('chatbot-msg').addClass('user-msg');
+      $('li:last-child .msg .msg-text p').empty();
+      let tmp = $('.user-textarea').val();
+      $('li:last-child .msg .msg-text p').append(tmp);
+      $('li:last-child .msg .message-time').empty();
+      let datetime = new Date().toLocaleTimeString();
+      $('li:last-child .msg .message-time').append(datetime);
+      $('.user-textarea').val('');
+    })
+    $('.user-textarea').on('keydown', function (e) {
+      var key = e.which;
+      if(key == 13) {
+          $('.submit-msg').click();
+          return false;
+      }
+    });
+  }
   
-  $('.chat-close').on('click',function(){
-    $('.chatbot-field').css('display','none');
-    $('.chat-textarea').css('box-shadow','none');
-    $('.chatbot-icon').css('display','block');
-  });
+}
+
+// export function chatbotFunc(){
+//   $('.chatbot-icon > img').on('click',function(){
+//     $('.chatbot-field').css('display','block');
+//     $('.chat-textarea').css('box-shadow','rgb(0 0 0 / 10%) 0px 5px 15px');
+//     $('.chatbot-icon').css('display','none');
+//   });
+    
+//   $('.chat-close').on('click',function(){
+//     $('.chatbot-field').css('display','none');
+//     $('.chat-textarea').css('box-shadow','none');
+//     $('.chatbot-icon').css('display','block');
+//   });
+// }
